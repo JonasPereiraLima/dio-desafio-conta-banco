10f34fea9a273083c42513f2f75adf715f5eaa84
Jonas <jonas.pereira.lima1@gmail.com>
2025-02-11 22:34:26 -0300

    adicionando resolucao do desafio de projeto da dio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite sua Agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da Agência");
+        int numeroAgencia = scanner.nextInt();
+
+        System.out.println("Digite o saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println(
+                "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "
+                        + numeroAgencia + " e seu saldo " + saldo + " já está disponível para saque");
+
     }
 }
